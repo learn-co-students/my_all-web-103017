ce60f8b81d522c9a648063e3945a3d7d8f1d30d7
jjwumaster <jjwumaster@gmail.com>
2017-11-10 14:01:51 -0600

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,4 +2,22 @@ require 'pry'
 
 def my_all?(collection)
 
+  block_return_values = []
+
+  i = 0
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i = i + 1
+  end
+
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
+
 end
+
+collection = [1,2,3,4,5]
+# binding.pry
+my_all?(collection){ |item| item < 3}
